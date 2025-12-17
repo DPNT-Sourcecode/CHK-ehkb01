8bf8ceca57020db6c257d06afa69822ab43ee37e
sbx_user1051 <sbx_user1051@169.254.62.51>
2025-12-17 17:36:12 +0000

    Wed Dec 17 17:36:12 UTC 2025

--- src/main/kotlin/solutions/DMO/DemoRound1Solution.kt
+++ src/main/kotlin/solutions/DMO/DemoRound1Solution.kt
@@ -4,10 +4,10 @@ import java.util.Locale.getDefault
 
 class DemoRound1Solution {
     fun sum(num1: Int, num2: Int): Int {
-        if (num1 !in 0..100) {
+        if (num1 !in 1..<100) {
             throw IllegalArgumentException("num1 is out of bounds")
         }
-        if (num2 !in 0..100) {
+        if (num2 !in 1..<100) {
             throw IllegalArgumentException("num2 is out of bounds")
         }
         return num1 + num2
