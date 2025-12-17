7999c635efbfdb82bb5f7669044c687421e5058c
sbx_user1051 <sbx_user1051@169.254.62.51>
2025-12-17 18:07:35 +0000

    Wed Dec 17 18:07:35 UTC 2025

--- src/main/kotlin/solutions/HLO/HelloSolution.kt
+++ src/main/kotlin/solutions/HLO/HelloSolution.kt
@@ -2,6 +2,7 @@ package solutions.HLO
 
 class HelloSolution {
     fun hello(friendName: String): String {
-        TODO("Solution not implemented")
+        return "Hello, $friendName!"
     }
 }
+
