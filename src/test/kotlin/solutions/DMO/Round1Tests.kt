46b288f7f1b1230c282eeca99dec897ee403ddb7
sbx_user1051 <sbx_user1051@169.254.62.51>
2025-12-17 17:24:12 +0000

    Wed Dec 17 17:24:12 UTC 2025

--- src/test/kotlin/solutions/DMO/Round1Tests.kt
+++ src/test/kotlin/solutions/DMO/Round1Tests.kt
@@ -1,9 +1,36 @@
 package solutions.DMO
 
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 
 class Round1Tests {
     @Test
-    @
-    fun sum()
+    @DisplayName("sum test with 2 correct numbers")
+    fun sum() {
+        val result = DemoRound1Solution().sum(10, 20)
+        Assertions.assertEquals(30, result)
+    }
+
+    @Test
+    @DisplayName("sum test with one number equal to 0")
+    fun sumWithZero() {
+        val result = DemoRound1Solution().sum(0, 15)
+        Assertions.assertEquals(15, result)
+    }
+
+    @Test
+    @DisplayName("sum test with one number equal to 100")
+    fun sumWithOneHundred() {
+        val result = DemoRound1Solution().sum(20, 100)
+        Assertions.assertEquals(120, result)
+    }
+
+    @Test
+    @DisplayName("sum test with one number out of bounds (negative)")
+    fun sumWithNegativeNumber() {
+        val result = DemoRound1Solution().sum(20, -1)
+        Assertions.assertEquals(120, result)
+    }
+
 }
