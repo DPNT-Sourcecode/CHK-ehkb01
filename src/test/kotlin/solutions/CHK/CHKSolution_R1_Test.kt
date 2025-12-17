30fb754202055e35ed650c57396a3325ded158ae
sbx_user1051 <sbx_user1051@169.254.62.51>
2025-12-17 20:45:55 +0000

    Wed Dec 17 20:45:55 UTC 2025

--- src/test/kotlin/solutions/CHK/CHKSolution_R1_Test.kt
+++ src/test/kotlin/solutions/CHK/CHKSolution_R1_Test.kt
@@ -54,4 +54,10 @@ class CHKSolution_R1_Test {
         Assertions.assertEquals(0, result)
     }
 
+    @Test
+    @DisplayName("chechout test with multiple special offers and remaining items")
+    fun checkoutWithMultipleSpecialOffersAndRemainingItems() {
+        val result = CheckoutSolution().checkout("AAAAAABBBCCD")
+        Assertions.assertEquals(390, result)
+    }
 }
